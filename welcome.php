568c75ba37a4679547c3a48f7b544c0f9679c062
eltonanit <elton.ani@gmail.com>
2024-09-06 19:09:15 +0200

    [UPD]Added welcome page

---
+++ welcome.php
@@ -0,0 +1,25 @@
+
+
+<?php 
+
+ 
+     $paragrafo = $_GET['paragrafo'];
+     $censura = $_GET['censura'];
+     echo "<pre>";
+     echo $nome, $cognome;
+     echo "</pre>";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+ <h1>Benvenuto <?php echo $nome." ".$cognome; ?> a casa</h1> 
+</body>
+</html>
